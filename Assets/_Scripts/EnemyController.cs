ab0011e2d5d2773c85a9f33a5d28ecf1526914bb
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2015-11-30 00:50:53 -0500

    Added Player (FPS), Enemy, Scripts
    EnemyController has MoveTowards code to follow Player, but goes through
    the mesh render of the map, need to use nav mesh or collision code.
    Collision code for PlayerController not going well, need to have when
    Enemy collides with player, then instant Game Over.
    
    Need to: Make a Bigger Map, Diff. Enemies with Working AI to follow
    player and shoot Projectiles (takes lives from the player when hit),
    make to Goal Post for Player to walk into (collide) and escape that
    level into the next one (also, have it spawn at different locations of
    the map each minute - Delta.time), need to have Audio, Pickups (for
    Scoring - for every 1000 reached, give 1 Life), need Score and Lives
    controller that doesn't destroy-on-load to have the same score and lives
    throughout levels.

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    // PUBLIC PROPERTIES
+    public Transform target;
+    public float speed;
+    public float triggerDistance;
+    // PRIVATE PROPERTIES
+    private float _targetDistance;
+    private Transform _transform;
+
+    // Use this for initialization
+    void Start()
+    {
+        this._transform = gameObject.GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._targetDistance = Vector3.Distance(this._transform.position, target.position);
+        if(this._targetDistance <= triggerDistance){
+        this._transform.position = Vector3.MoveTowards(this._transform.position, target.position, speed);
+        }
+        Debug.Log(this._targetDistance);
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public int Lives;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy")) // If the other GameObject that collided with the player - is Enemy -
+        {
+            Debug.Log("Enemy Hit");
+            Destroy(other.gameObject); //Test - Collision with the Enemy will be a Game Over
+        }
+    }
+}
