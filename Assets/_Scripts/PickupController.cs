5c4ef46f9cca4640a086df3061e30b76284b9ed2
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2015-12-01 19:51:20 -0500

    Added Game States, Pickups, Timer, Labels
    Redesign for this Game:
    
    Pick up 15 Sandler Spheres within an a set time frame. If the time limit
    is reached without picking up all of the Sandlers, you lose; if you get
    15 Sandlers within the time limit, you win.
    
    What's left to do:
    
    Add Audio, set a certain time limit that is acceptable for a challenge,
    have a score controller and time controller to bring the number of the
    acquired pickups to Game Over state, and the best time run to the Win
    state.

---
+++ Assets/_Scripts/PickupController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupController : MonoBehaviour {
+
+    public Transform[] pickupSpawns; // an array transform components of the coinSpawn empty gameobjects
+    public GameObject pickup; // reference to hold coin gameObject
+
+    // Use this for initialization
+    void Start()
+    {
+        Spawn(); // at first frame, call Spawn method
+    }
+
+    void Spawn() // method that spawns coins on a loop the size of the transform array
+    {
+        for (int i = 0; i < pickupSpawns.Length; i++)
+        {
+                Instantiate(pickup, pickupSpawns[i].position, pickupSpawns[i].rotation); // instantiate coin gameObject, at the coinSpawn tranform of the current loop index
+        }
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,26 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System;
 
 public class PlayerController : MonoBehaviour {
 
-    public int Lives;
+    public int count;
+
+    public Text aquiredLabel;
+    public Text timerLabel;
+    public Text titleLabel;
+    public Text instructLabel;
+
+    public float deathTime;
+    private float titleTime = 5;
+    private float[] ticking = {5,6,7,8,9};
 
 	// Use this for initialization
 	void Start () {
-	
+        SetCount();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if(Time.time >= titleTime)
+        {
+            this.titleLabel.enabled = false;
+            this.instructLabel.enabled = false;
+        }
+        Timer();
 	}
 
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Enemy")) // If the other GameObject that collided with the player - is Enemy -
+        if (other.gameObject.CompareTag("Pickup")) // If the other GameObject that collided with the player - is Enemy -
         {
-            Debug.Log("Enemy Hit");
+            count++;
+            SetCount();
+            Debug.Log("Adam Sandler Acquired");
             Destroy(other.gameObject); //Test - Collision with the Enemy will be a Game Over
+        }
+    }
+
+    public void SetCount() // method to update to the current score upon killing enemies or picking up Gold and Silver coins
+    {
+        this.aquiredLabel.text = "Acquired: " + count + "/15"; // label equals this string statement - score is concatenated to string for display
+    }
+    public void Timer () // method to update to the current score upon killing enemies or picking up Gold and Silver coins
+    {
+        this.timerLabel.text = String.Format("{0:0}", Time.time); // label equals this string statement - score is concatenated to string for display
+
+        if (Time.time >= ticking[0])
+        {
+            this.timerLabel.color = Color.red;
+            if (Time.time >= ticking[1])
+            {
+            }
+            if (Time.time >= ticking[2])
+            {
+            }
+            if (Time.time >= ticking[3])
+            {
+            }
+            if (Time.time >= ticking[4])
+            {
+            }
+        }
+
+        if (Time.time >= deathTime)
+        {
+            Application.LoadLevel("Game Over");
         }
     }
 }
